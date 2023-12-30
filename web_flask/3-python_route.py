538c37c26f5a87686b025758878a53b823cc9d28
MphoCM <christian.mofokeng@gmail.com>
2023-12-30 06:53:10 -0800

    3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,42 +1,35 @@
-#!/usr/bin/python#
-""" This application listens on 0.0.0.0, port 5000.
-Routes:
-/: display "'Hello HBNB"
-/hbnh: display "HBNB!"
-/c/<text>: display “C ” followed by the value\
-    of the text variable\
-        (replace underscore _ symbols with a space )
-"""
+#!/usr/bin/python3
+"""Start a Flask web application"""
+
 from flask import Flask
+from werkzeug.utils import escape
 
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def home():
-    """Displays 'Hello HBNB!'"""
-    return "Hello HBNB!"
-
-
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """/hbnb route : display HBNB"""
-    return "HBNB"
+    """Display 'Hello HBNB!'"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Display 'HBNB'"""
+    return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def dynamic_route(text):
-    """Displays 'C' followed by the value of <text>."""
-    text = text.replace("_", " ")
-    return f"C {text}"
+def c_route(text):
+    """Display 'C ' followed by the value of the text variable"""
+    return 'C {}'.format(escape(text).replace('_', ' '))
 
 
-@app.route('/python/')
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def dynamic_route_pyton(text="is cool"):
-    """Displays 'C' followed by the value of <text>."""
-    text = text.replace("_", " ")
-    return f"Python {text}"
+def python_route(text):
+    """Display 'Python ' followed by the value of the text variable"""
+    return 'Python {}'.format(escape(text).replace('_', ' '))
 
 
 if __name__ == "__main__":
